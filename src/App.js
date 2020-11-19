2e3fd9c86579f9c3a0c68f02fc7b66af80558e11
Ramari Heperi <ramarimei@iMac-2.developersdomain>
2020-11-19 17:33:38 +1300

    css grid coordintates

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import Logo from "./images/Mu_Torere.svg";
 import "./App.css";
 
 function App() {
-  return (
+ return (
 <div class="container">
-  <div class="square1"></div>
-  <div class="square2"></div>
-  <div class="square3"></div>
-  <div class="square4"></div>
-  <div class="square5"></div>
-  <div class="square6"></div>
-  <div class="square7"></div>
-  <div class="square8"></div>
-  <div class="center"></div>
+ 
 </div>
-  );
-}
-
+ )};
+ 
 export default App;
