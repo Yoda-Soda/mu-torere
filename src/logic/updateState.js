66803bc5cb03873e22ed46eb148968e643644ed7
Jan <janclaasen@gmail.com>
2020-11-20 01:17:11 +1300

    added function to find open space on board

--- src/logic/updateState.js
+++ src/logic/updateState.js
@@ -10,22 +10,21 @@ const resetState = () => {
     { posistion: 6, team: "black", enabled: false },
     { posistion: 7, team: "black", enabled: false },
     { posistion: 8, team: "black", enabled: false },
-    { posistion: 9, team: "black", enabled: false },
   ];
 };
 // allows for predictable starting state for the start of the game
 const startingState = () => {
   return [
     { turn: "white" },
+    { posistion: 0, team: "black", enabled: false },
     { posistion: 1, team: "black", enabled: false },
     { posistion: 2, team: "black", enabled: false },
     { posistion: 3, team: "black", enabled: false },
-    { posistion: 4, team: "black", enabled: false },
-    { posistion: 5, team: "white", enabled: true },
+    { posistion: 4, team: "white", enabled: true },
+    { posistion: 5, team: "white", enabled: false },
     { posistion: 6, team: "white", enabled: false },
-    { posistion: 7, team: "white", enabled: false },
-    { posistion: 8, team: "white", enabled: true },
-    { posistion: 9, team: null, enabled: true },
+    { posistion: 7, team: "white", enabled: true },
+    { posistion: 8, team: null, enabled: true },
   ];
 };
 
@@ -34,6 +33,13 @@ const togglePlayer = (playerState) => {
   return "black";
 };
 
-const findNull = (state) => {};
+const findOpenPosition = (state) => {
+  for (let counter = 1; counter < state.length; counter++) {
+    const element = state[counter];
+    if (element.team === null) {
+      return counter - 1;
+    }
+  }
+};
 
-module.exports = { resetState, startingState, togglePlayer };
+module.exports = { resetState, startingState, togglePlayer, findOpenPosition };
