9f504b5cf96f6de055987b30e0c88dd75b7af5db
NMaree <61126225+NMaree@users.noreply.github.com>
2020-11-19 20:42:02 +1300

    responsive image

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import Logo from "./images/Mu_Torere.svg";
 import "./App.css";
 
 function App() {
- return (
-<div class="container">
- 
-</div>
- )};
- 
+  return (
+    <div class="container">
+      <div class="board">
+        <button class="square white pos1"></button>
+        <button class="square white pos2"></button>
+        <button class="square black pos3"></button>
+        <button class="square black pos4"></button>
+        <button class="square black pos5"></button>
+        <button class="square white pos6"></button>
+        <button class="square white pos7"></button>
+        <button class="square black pos8"></button>
+        <button class="square black pos9"></button>
+      </div>
+    </div>
+  );
+}
+
 export default App;
