85a01d6833d05cd52db572498e987b7c269c7241
Ramari Heperi <ramarimei@iMac-2.developersdomain>
2020-11-19 16:16:42 +1300

    grid layout on top of the game board

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <img src={Logo} alt="board_game image" />
-    </div>
+<div class="container">
+  <div class="square1"></div>
+  <div class="square2"></div>
+  <div class="square3"></div>
+  <div class="square4"></div>
+  <div class="square5"></div>
+  <div class="square6"></div>
+  <div class="square7"></div>
+  <div class="square8"></div>
+  <div class="center"></div>
+</div>
   );
 }
 
