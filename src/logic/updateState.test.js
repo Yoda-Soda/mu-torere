ddd178f002ef0a4c2a8dfcecfd58ebf799a5f687
Jan <janclaasen@gmail.com>
2020-11-20 00:28:26 +1300

    added posistion to state

--- src/logic/updateState.test.js
+++ src/logic/updateState.test.js
@@ -5,15 +5,15 @@ describe("I have a way to reset my state", () => {
     const state = resetState();
     expect(state).toEqual([
       { turn: "black" },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
+      { posistion: 0, team: "black", enabled: false },
+      { posistion: 1, team: "black", enabled: false },
+      { posistion: 3, team: "black", enabled: false },
+      { posistion: 4, team: "black", enabled: false },
+      { posistion: 5, team: "black", enabled: false },
+      { posistion: 6, team: "black", enabled: false },
+      { posistion: 7, team: "black", enabled: false },
+      { posistion: 8, team: "black", enabled: false },
+      { posistion: 9, team: "black", enabled: false },
     ]);
   });
 });
@@ -23,15 +23,15 @@ describe("I have a way to start my state", () => {
     const state = startingState();
     expect(state).toEqual([
       { turn: "white" },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "black", enabled: false },
-      { team: "white", enabled: true },
-      { team: "white", enabled: false },
-      { team: "white", enabled: false },
-      { team: "white", enabled: true },
-      { team: null, enabled: true },
+      { posistion: 1, team: "black", enabled: false },
+      { posistion: 2, team: "black", enabled: false },
+      { posistion: 3, team: "black", enabled: false },
+      { posistion: 4, team: "black", enabled: false },
+      { posistion: 5, team: "white", enabled: true },
+      { posistion: 6, team: "white", enabled: false },
+      { posistion: 7, team: "white", enabled: false },
+      { posistion: 8, team: "white", enabled: true },
+      { posistion: 9, team: null, enabled: true },
     ]);
   });
 });
