ad5b9bf31730664db306689a9a254e90533a27df
Jan <janclaasen@gmail.com>
2020-11-20 00:09:48 +1300

    added enabled state

--- src/logic/updateState.js
+++ src/logic/updateState.js
@@ -2,30 +2,30 @@
 const resetState = () => {
   return [
     { turn: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
   ];
 };
 // allows for predictable starting state for the start of the game
 const startingState = () => {
   return [
     { turn: "white" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "black" },
-    { team: "white" },
-    { team: "white" },
-    { team: "white" },
-    { team: "white" },
-    { team: null },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "black", enabled: false },
+    { team: "white", enabled: true },
+    { team: "white", enabled: false },
+    { team: "white", enabled: false },
+    { team: "white", enabled: true },
+    { team: null, enabled: true },
   ];
 };
 
--- src/logic/updateState.test.js
+++ src/logic/updateState.test.js
@@ -1,19 +1,19 @@
-const { resetState, startingState } = require("./updateState");
+const { resetState, startingState, togglePlayers } = require("./updateState");
 
 describe("I have a way to reset my state", () => {
   test("It should restore the state to a predictable state", () => {
     const state = resetState();
     expect(state).toEqual([
       { turn: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
     ]);
   });
 });
@@ -23,15 +23,15 @@ describe("I have a way to start my state", () => {
     const state = startingState();
     expect(state).toEqual([
       { turn: "white" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "black" },
-      { team: "white" },
-      { team: "white" },
-      { team: "white" },
-      { team: "white" },
-      { team: null },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "black", enabled: false },
+      { team: "white", enabled: true },
+      { team: "white", enabled: false },
+      { team: "white", enabled: false },
+      { team: "white", enabled: true },
+      { team: null, enabled: true },
     ]);
   });
 });
