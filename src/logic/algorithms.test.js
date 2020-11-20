dbfe72c93dd6d4987670e5c4520e008e81828155
Jan <janclaasen@gmail.com>
2020-11-21 09:39:19 +1300

    added test to check test for tokens being disabled if no oppossit adjacent token has been found

--- src/logic/algorithms.test.js
+++ src/logic/algorithms.test.js
@@ -1,4 +1,8 @@
-const { findOpenPosition, checkAndSetAllSurounded } = require("./algorithms");
+const {
+  findOpenPosition,
+  checkAndSetAllSurounded,
+  updateForAdjacentOpponent,
+} = require("./algorithms");
 
 describe("I have a way of finding the open/null position on the board", () => {
   const testState = {
@@ -83,6 +87,68 @@ describe("I have a way to check if a token is surounded by other tokens and can'
         { position: 6, team: "black", enabled: false },
         { position: 7, team: "black", enabled: true },
         { position: 8, team: "black", enabled: true },
+      ],
+    });
+  });
+});
+describe("I have a way to check if a token is surounded by at least one opponent token", () => {
+  test(`It should not disable that token if opponent token has been found adjacent to it`, () => {
+    const state = updateForAdjacentOpponent({
+      turn: "black",
+      pereperes: [
+        { position: 0, team: "black", enabled: true },
+        { position: 1, team: "black", enabled: true },
+        { position: 2, team: "black", enabled: true },
+        { position: 3, team: "black", enabled: true },
+        { position: 4, team: "black", enabled: true },
+        { position: 5, team: "black", enabled: true },
+        { position: 6, team: "black", enabled: true },
+        { position: 7, team: "black", enabled: true },
+        { position: 8, team: "black", enabled: true },
+      ],
+    });
+    expect(state).toEqual({
+      turn: "black",
+      pereperes: [
+        { position: 0, team: "black", enabled: false },
+        { position: 1, team: "black", enabled: false },
+        { position: 2, team: "black", enabled: false },
+        { position: 3, team: "black", enabled: false },
+        { position: 4, team: "black", enabled: false },
+        { position: 5, team: "black", enabled: false },
+        { position: 6, team: "black", enabled: false },
+        { position: 7, team: "black", enabled: false },
+        { position: 8, team: "black", enabled: false },
+      ],
+    });
+  });
+  test(`It should disable that token if opponent token has been found adjacent to it`, () => {
+    const state = updateForAdjacentOpponent({
+      turn: "black",
+      pereperes: [
+        { position: 0, team: "black", enabled: true },
+        { position: 1, team: "black", enabled: true },
+        { position: 2, team: "black", enabled: true },
+        { position: 3, team: "black", enabled: true },
+        { position: 4, team: "black", enabled: true },
+        { position: 5, team: "black", enabled: true },
+        { position: 6, team: "black", enabled: true },
+        { position: 7, team: "black", enabled: true },
+        { position: 8, team: "white", enabled: true },
+      ],
+    });
+    expect(state).toEqual({
+      turn: "black",
+      pereperes: [
+        { position: 0, team: "black", enabled: true },
+        { position: 1, team: "black", enabled: true },
+        { position: 2, team: "black", enabled: true },
+        { position: 3, team: "black", enabled: true },
+        { position: 4, team: "black", enabled: true },
+        { position: 5, team: "black", enabled: true },
+        { position: 6, team: "black", enabled: true },
+        { position: 7, team: "black", enabled: true },
+        { position: 8, team: "white", enabled: true },
       ],
     });
   });
