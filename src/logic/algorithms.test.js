b0348ae087078ad2ce44d10b07dd147fb34fb3fa
Jan <janclaasen@gmail.com>
2020-11-21 00:02:45 +1300

    add test for only some surounded tokens to be disabled

--- src/logic/algorithms.test.js
+++ src/logic/algorithms.test.js
@@ -26,7 +26,7 @@ describe("I have a way of finding the open/null position on the board", () => {
 });
 
 describe("I have a way to check if a token is surounded by other tokens and can't move", () => {
-  test(`It should disable a token`, () => {
+  test(`It should disable all tokens`, () => {
     const state = checkAndSetAllSurounded({
       turn: "black",
       pereperes: [
@@ -53,6 +53,36 @@ describe("I have a way to check if a token is surounded by other tokens and can'
         { position: 6, team: "black", enabled: false },
         { position: 7, team: "black", enabled: false },
         { position: 8, team: "black", enabled: false },
+      ],
+    });
+  });
+  test(`It should disable some tokens`, () => {
+    const state = checkAndSetAllSurounded({
+      turn: "black",
+      pereperes: [
+        { position: 0, team: null, enabled: true },
+        { position: 1, team: "black", enabled: true },
+        { position: 2, team: "black", enabled: true },
+        { position: 3, team: "black", enabled: true },
+        { position: 4, team: "black", enabled: true },
+        { position: 5, team: "black", enabled: true },
+        { position: 6, team: "black", enabled: true },
+        { position: 7, team: "black", enabled: true },
+        { position: 8, team: "black", enabled: true },
+      ],
+    });
+    expect(state).toEqual({
+      turn: "black",
+      pereperes: [
+        { position: 0, team: null, enabled: false },
+        { position: 1, team: "black", enabled: true },
+        { position: 2, team: "black", enabled: false },
+        { position: 3, team: "black", enabled: false },
+        { position: 4, team: "black", enabled: false },
+        { position: 5, team: "black", enabled: false },
+        { position: 6, team: "black", enabled: false },
+        { position: 7, team: "black", enabled: true },
+        { position: 8, team: "black", enabled: true },
       ],
     });
   });
